1a671b576ef2f8a3fb1c6f5515c76561409e2354
KS Sreeram <sreeram@tachyontech.net>
2009-07-17 22:36:17 +0530

    implemented astprinter.py using xprint.

---
+++ clay/astprinter.py
@@ -0,0 +1,59 @@
+from clay.ast import *
+from clay.xprint import xprint, XObject, XField, XSymbol, xregister
+
+__all__ = ["ast_print"]
+
+def ast_print(x) :
+    xprint(x)
+
+
+r = xregister
+xo = XObject
+xf = XField
+
+
+#
+# program
+#
+
+r(Program, lambda x : xo("Program", *x.top_level_items))
+
+
+#
+# top level items
+#
+
+r(RecordDef, lambda x : xo("RecordDef", x.name, xf("type_vars",x.type_vars),
+                           *x.fields)
+r(Field, lambda x : xf(x.name,x.type,separator=":"))
+r(VariableDef, lambda x : xo("VariableDef", x.name, x.expr))
+r(ProcedureDef, lambda x : xo("ProcedureDef", x.name, tuple(x.args), x.body))
+
+
+#
+# statements
+#
+
+r(Block, lambda x : xo("Block", *x.statements))
+r(LocalVariableDef, lambda x : xo("LocalVariableDef", x.name, x.expr))
+r(Assignment, lambda x : xo("Assignment", x.name, x.expr))
+r(IfStatement, lambda x : xo("IfStatement", x.condition,
+                             xf("then", x.then_part),
+                             xf("else", x.else_part)))
+r(ReturnStatement, lambda x : xo("ReturnStatement", x.expr))
+
+
+#
+# expressions
+#
+
+r(IndexExpr, lambda x : xo("IndexExpr", x.expr, xf("indexes",x.indexes)))
+r(CallExpr, lambda x : xo("CallExpr", x.expr, xf("args",x.args)))
+r(FieldRef, lambda x : xo("FieldRef", x.expr, xf("field",x.name)))
+r(ArrayExpr, lambda x : xo("ArrayExpr", *x.elements))
+r(TupleExpr, lambda x : xo("TupleExpr", *x.elements))
+r(NameRef, lambda x : xo("NameRef", x.name))
+r(BoolLiteral, lambda x : xo("BoolLiteral", x.value))
+r(IntLiteral, lambda x : xo("IntLiteral", x.value))
+r(CharLiteral, lambda x : xo("CharLiteral", x.value))
+r(StringLiteral, lambda x : xo("StringLiteral", x.value))
