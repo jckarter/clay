7fefd4305ac68ed0d4cfb1a68c85fbb7e70e45f3
KS Sreeram <sreeram@tachyontech.net>
2009-07-18 23:27:57 +0530

    fixed couple of bugs in linecol.

--- clay/linecol.py
+++ clay/linecol.py
@@ -1,9 +1,9 @@
 def locate_line_column(data, pos) :
     line = 1
-    for s in data.splitlines() :
+    for s in data.splitlines(True) :
         if pos < len(s) :
             break
         pos -= len(s)
         line += 1
-    column = pos + 1
+    column = pos
     return line, column
