b7305599d500bd0e480d4c076b72343ee6533ddb
KS Sreeram <sreeram@tachyontech.net>
2009-07-25 13:20:32 +0530

    added support for a default return value in multimethod.

--- clay/multimethod.py
+++ clay/multimethod.py
@@ -1,12 +1,15 @@
 __all__ = ["multimethod"]
 
-def multimethod(n=1) :
-    return MultiMethod(n)
+class DummyDefault(object) : pass
+
+def multimethod(n=1, default=DummyDefault) :
+    return MultiMethod(n, default)
 
 class MultiMethod(object) :
-    def __init__(self, n) :
+    def __init__(self, n, default) :
         self.registry_ = {}
         self.n_ = n
+        self.default_ = default
 
     def add_handler(self, f, *types) :
         self.registry_[types] = f
@@ -19,5 +22,8 @@ class MultiMethod(object) :
 
     def __call__(self, *args) :
         types = tuple(type(x) for x in args[:self.n_])
-        handler = self.registry_[types]
+        handler = self.registry_.get(types)
+        if handler is None :
+            assert self.default_ is not DummyDefault
+            return self.default_
         return handler(*args)
