8051ba86a45cf548c4f286a045a3be8aa3a83bb7
KS Sreeram <sreeram@tachyontech.net>
2009-07-26 20:39:17 +0530

    implemented type unification and a couple of minor changes.

--- clay/types.py
+++ clay/types.py
@@ -1,13 +1,14 @@
 from clay.multimethod import multimethod
 
-__all__ = ["Type", "PrimitiveType", "bool_type", "int_type", "char_type",
-           "void_type", "TupleType", "ArrayType", "ArrayValueType", "RefType",
+__all__ = ["Type", "bool_type", "int_type", "char_type", "void_type",
+           "TupleType", "ArrayType", "ArrayValueType", "RefType",
            "RecordType", "StructType",
-           "type_equals",
            "is_primitive_type", "is_bool_type", "is_int_type", "is_char_type",
            "is_void_type", "is_tuple_type", "is_array_type",
            "is_array_value_type", "is_ref_type",
-           "is_record_type", "is_struct_type"]
+           "is_record_type", "is_struct_type",
+           "type_equals", "type_list_equals",
+           "type_unify", "type_list_unify", "TypeVariable"]
 
 
 
@@ -53,6 +54,24 @@ class StructType(Type) :
     def __init__(self, struct_entry, type_params) :
         self.struct_entry = struct_entry
         self.type_params = type_params
+
+
+
+#
+# type predicates
+#
+
+def is_primitive_type(t) : return type(t) is PrimitiveType
+def is_bool_type(t) : return t is bool_type
+def is_int_type(t) : return t is int_type
+def is_char_type(t) : return t is char_type
+def is_void_type(t) : return t is void_type
+def is_tuple_type(t) : return type(t) is TupleType
+def is_array_type(t) : return type(t) is ArrayType
+def is_array_value_type(t) : return type(t) is ArrayValueType
+def is_ref_type(t) : return type(t) is RefType
+def is_record_type(t) : return type(t) is RecordType
+def is_struct_type(t) : return type(t) is StructType
 
 
 
@@ -103,17 +122,76 @@ def f(x, y) :
 
 
 #
-# type predicates
+# type unification
 #
 
-def is_primitive_type(t) : return type(t) is PrimitiveType
-def is_bool_type(t) : return type_equals(t, bool_type)
-def is_int_type(t) : return type_equals(t, int_type)
-def is_char_type(t) : return type_equals(t, char_type)
-def is_void_type(t) : return type_equals(t, void_type)
-def is_tuple_type(t) : return type(t) is TupleType
-def is_array_type(t) : return type(t) is ArrayType
-def is_array_value_type(t) : return type(t) is ArrayValueType
-def is_ref_type(t) : return type(t) is RefType
-def is_record_type(t) : return type(t) is RecordType
-def is_struct_type(t) : return type(t) is StructType
+class TypeVariable(Type) :
+    def __init__(self) :
+        self.type = None
+
+    def deref(self) :
+        if type(self.type) is TypeVariable :
+            return self.type.deref()
+        return self.type
+
+
+type_unify = multimethod(n=2, default_proc=type_unify_variables)
+
+def type_list_unify(a, b) :
+    if len(a) != len(b) :
+        return False
+    for x,y in zip(a,b) :
+        if not type_unify(x,y) :
+            return False
+    return True
+
+def type_unify_variables(x, y) :
+    if type(x) is TypeVariable :
+        if x.type is None :
+            x.type = y
+            return True
+        return type_unify(x.type, y)
+    elif type(y) is TypeVariable :
+        if y.type is None :
+            y.type = x
+            return True
+        return type_unify(x, y.type)
+    assert False
+
+@type_unify.register(PrimitiveType, PrimitiveType)
+def f(x, y) :
+    return x.name == y.name
+
+@type_unify.register(TupleType, TupleType)
+def f(x, y) :
+    return type_list_unify(x.types, y.types)
+
+@type_unify.register(ArrayType, ArrayType)
+def f(x, y) :
+    return type_unify(x.type, y.type)
+
+@type_unify.register(ArrayValueType, ArrayValueType)
+def f(x, y) :
+    return (x.size == y.size) and type_unify(x.type, y.type)
+
+@type_unify.register(RefType, RefType)
+def f(x, y) :
+    return type_unify(x.type, y.type)
+
+@type_unify.register(RecordType, RecordType)
+def f(x, y) :
+    return (x.record_entry is y.record_entry) and \
+        type_list_unify(x.type_params, y.type_params)
+
+@type_unify.register(StructType, StructType)
+def f(x, y) :
+    return (x.struct_entry is y.struct_entry) and \
+        type_list_unify(x.type_params, y.type_params)
+
+
+
+#
+# remove temp name used for multimethod instances
+#
+
+del f
