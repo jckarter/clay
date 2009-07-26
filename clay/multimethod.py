e32c2f8cb05980c54b1cf0fe97af2bd635ac112a
KS Sreeram <sreeram@tachyontech.net>
2009-07-26 20:36:31 +0530

    added support default_proc support for multimethods.

--- clay/multimethod.py
+++ clay/multimethod.py
@@ -2,14 +2,25 @@ __all__ = ["multimethod"]
 
 class DummyDefault(object) : pass
 
-def multimethod(n=1, default=DummyDefault) :
-    return MultiMethod(n, default)
+class DefaultProc(object) :
+    def __init__(self, proc) :
+        self.proc = proc
+
+def multimethod(n=1, default=DummyDefault, default_proc=DummyDefault) :
+    assert (default is DummyDefault) or (default_proc is DummyDefault)
+    if default is not DummyDefault :
+        default_proc = lambda *args : default
+    elif default_proc is not DummyDefault :
+        pass
+    else :
+        default_proc = None
+    return MultiMethod(n, default_proc)
 
 class MultiMethod(object) :
-    def __init__(self, n, default) :
+    def __init__(self, n, default_proc) :
         self.registry_ = {}
         self.n_ = n
-        self.default_ = default
+        self.default_proc_ = default_proc
 
     def add_handler(self, f, *types) :
         self.registry_[types] = f
@@ -22,8 +33,5 @@ class MultiMethod(object) :
 
     def __call__(self, *args) :
         types = tuple(type(x) for x in args[:self.n_])
-        handler = self.registry_.get(types)
-        if handler is None :
-            assert self.default_ is not DummyDefault
-            return self.default_
+        handler = self.registry_.get(types, self.default_proc_)
         return handler(*args)
