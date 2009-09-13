a4016b227499df4d6a3ecd94b727f8c04351d790
KS Sreeram <sreeram@tachyontech.net>
2009-09-13 20:52:00 +0530

    added code to test the compiler.

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 import sys
 from clay.xprint import xprint
-from clay.error import CompilerError
+from clay.error import CompilerError, error
 from clay.parser import parse
 from clay.ast import *
 from clay.env import buildTopLevelEnv
 from clay.coreops import cleanupGlobals
-from clay.evaluator import evaluate
+from clay.evaluator import evaluate, InvokeBindings
 from clay.analyzer import analyze
-from clay.compiler import llvmType
+from clay import compiler
+from clay import llvmwrapper as llvm
 
-def process() :
+def testAnalyzeEval() :
     fileName = sys.argv[1]
     data = file(fileName).read()
     try :
@@ -20,8 +21,24 @@ def process() :
         xprint(result)
         result = evaluate(mainCall, env)
         xprint(result)
-        llt = llvmType(result.type)
+        llt = compiler.llvmType(result.type)
         print "llvm type =", llt
+    except CompilerError, e :
+        e.display()
+        raise
+
+def process() :
+    fileName = sys.argv[1]
+    data = file(fileName).read()
+    try :
+        program = parse(data, fileName)
+        env = buildTopLevelEnv(program)
+        mainProc = env.lookup("main")
+        if type(mainProc) is not Procedure :
+            error("'main' is not a procedure")
+        bindings = InvokeBindings([], [], [], [])
+        compiler.compileCode("main", mainProc.code, mainProc.env, bindings)
+        print str(compiler.llvmModule)
     except CompilerError, e :
         e.display()
         raise
@@ -34,4 +51,3 @@ def main() :
 
 if __name__ == "__main__" :
     main()
-    print "done"
