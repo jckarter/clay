b164a6d8e9beab6c1fe103d953c407c5c6b95df2
KS Sreeram <sreeram@tachyontech.net>
2010-03-01 01:36:39 +0530

    symbol for initializing assignment changed from "<==" to "<--".

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -133,7 +133,7 @@ static bool keywordIdentifier(TokenPtr &x) {
 //
 
 static const char *symbols[] = {
-    "<==",
+    "<--",
     "==", "!=", "<=", ">=",
     "<", ">",
     "+=", "-=", "*=", "/=", "%=",
