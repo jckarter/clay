99170e8c992ce54dc24dba75141959af6cedf78f
KS Sreeram <sreeram@tachyontech.net>
2010-04-08 01:33:39 +0530

    updated runtests.py. added test/test23.clay as a basic exceptions test.

--- runtests.py
+++ runtests.py
@@ -4,7 +4,7 @@ import os
 tests = ("factorial platform " +
          "test1 test2 test3 test4 test5 test6 test7 test8 test9 test10 " +
          "test11 test12 test13 test14 test15 test16 test17 " +
-         "test18 test19 test20 test21 test22").split()
+         "test18 test19 test20 test21 test22 test23 exception").split()
 
 perfTests = ("insertionsort1 insertionsort2 insertionsort3 " +
              "mean quicksort1 quicksort2").split()
