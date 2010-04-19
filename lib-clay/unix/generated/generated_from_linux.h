ba328af7f944c6c560b809f601fb063d0028d611
KS Sreeram <sreeram@tachyontech.net>
2010-04-19 17:19:36 +0530

    added unix api bindings.

---
+++ lib-clay/unix/generated/generated_from_linux.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/epoll.h>
---
+++ lib-clay/unix/generated/generated_from_macosx.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/event.h>
