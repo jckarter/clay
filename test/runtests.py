03e7fea5524c6bc78aee5b2c746e99daa1e566ba
KS Sreeram <sreeram@tachyontech.net>
2010-04-29 17:20:32 +0530

    in lib-clay/paged, implemented named scalars.

--- test/runtests.py
+++ test/runtests.py
@@ -6,7 +6,7 @@ tests = ("factorial platform " +
          "test11 test12 test13 test14 test15 test16 test17 " +
          "test18 test19 test20 test21 test22 test23 exception " +
          "test24 test25 test26 test27 test28 test29 test30 " +
-         "test31 test32 test33 test34 test35 test36").split()
+         "test31 test32 test33 test34 test35 test36 test37").split()
 
 perfTests = ("insertionsort1 insertionsort2 " +
              "mean quicksort1 quicksort2").split()
