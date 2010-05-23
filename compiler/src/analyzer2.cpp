7e8b18a2a7f4c00b8d2a0dda7e69a5a1730766ee
KS Sreeram <sreeram@tachyontech.net>
2010-05-24 00:55:35 +0530

    new analyzer in progress.

---
+++ compiler/src/analyzer2.cpp
@@ -0,0 +1,493 @@
+#include "clay.hpp"
+
+namespace two {
+
+PValuePtr staticPValue(ObjectPtr x);
+PValuePtr kernelPValue(const string &name);
+
+MultiPValuePtr analyzeMulti(const vector<ExprPtr> &exprs, EnvPtr env);
+PValuePtr analyzeOne(ExprPtr expr, EnvPtr env);
+MultiPValuePtr analyzeExpr(ExprPtr expr, EnvPtr env);
+MultiPValuePtr analyzeStaticObject(ObjectPtr x);
+PValuePtr analyzeGlobalVariable(GlobalVariablePtr x);
+PValuePtr analyzeExternalVariable(ExternalVariablePtr x);
+void analyzeExternalProcedure(ExternalProcedurePtr x);
+MultiPValuePtr analyzeIndexingExpr(ExprPtr indexable,
+                                   const vector<ExprPtr> &args,
+                                   EnvPtr env);
+MultiPValuePtr analyzeFieldRefExpr(ExprPtr base,
+                                   IdentifierPtr name,
+                                   EnvPtr env);
+MultiPValuePtr analyzeCallExpr(ExprPtr callable,
+                               const vector<ExprPtr> &args,
+                               EnvPtr env);
+
+MultiPValuePtr analyzeCallValue(PValuePtr callable,
+                                MultiPValuePtr args);
+
+
+
+//
+// staticPValue, kernelPValue
+//
+
+PValuePtr staticPValue(ObjectPtr x)
+{
+    TypePtr t = staticType(x);
+    return new PValue(t, true);
+}
+
+PValuePtr kernelPValue(const string &name)
+{
+    return staticPValue(kernelName(name));
+}
+
+
+
+//
+// analyzeMulti
+//
+
+MultiPValuePtr analyzeMulti(const vector<ExprPtr> &exprs, EnvPtr env)
+{
+    MultiPValuePtr out = new MultiPValue();
+    for (unsigned i = 0; i < exprs.size(); ++i) {
+        ExprPtr x = exprs[i];
+        if (x->exprKind == VAR_ARGS_REF) {
+            MultiPValuePtr y = analyzeExpr(x, env);
+            if (!y)
+                return NULL;
+            out->values.insert(out->values.end(),
+                               y->values.begin(),
+                               y->values.end());
+        }
+        else {
+            PValuePtr y = two::analyzeOne(x, env);
+            if (!y)
+                return NULL;
+            out->values.push_back(y);
+        }
+    }
+    return out;
+}
+
+
+
+//
+// analyzeOne
+//
+
+PValuePtr analyzeOne(ExprPtr expr, EnvPtr env)
+{
+    MultiPValuePtr x = analyzeExpr(expr, env);
+    if (!x)
+        return NULL;
+    if (x->size() != 1)
+        arityError(expr, 1, x->size());
+    return x->values[0];
+}
+
+
+
+//
+// analyzeExpr
+//
+
+MultiPValuePtr analyzeExpr(ExprPtr expr, EnvPtr env)
+{
+    switch (expr->exprKind) {
+
+    case BOOL_LITERAL : {
+        return new MultiPValue(new PValue(boolType, true));
+    }
+
+    case INT_LITERAL : {
+        IntLiteral *x = (IntLiteral *)expr.ptr();
+        ValueHolderPtr v = parseIntLiteral(x);
+        return new MultiPValue(new PValue(v->type, true));
+    }
+        
+    case FLOAT_LITERAL : {
+        FloatLiteral *x = (FloatLiteral *)expr.ptr();
+        ValueHolderPtr v = parseFloatLiteral(x);
+        return new MultiPValue(new PValue(v->type, true));
+    }
+
+    case CHAR_LITERAL : {
+        CharLiteral *x = (CharLiteral *)expr.ptr();
+        if (!x->desugared)
+            x->desugared = desugarCharLiteral(x->value);
+        return analyzeExpr(x->desugared, env);
+    }
+
+    case STRING_LITERAL : {
+        StringLiteral *x = (StringLiteral *)expr.ptr();
+        PValuePtr pv = new PValue(arrayType(int8Type, x->value.size()), true);
+        MultiPValuePtr args = new MultiPValue(pv);
+        return analyzeCallValue(kernelPValue("stringRef"), args);
+    }
+
+    case NAME_REF : {
+        NameRef *x = (NameRef *)expr.ptr();
+        ObjectPtr y = lookupEnv(env, x->name);
+        if (y->objKind == EXPRESSION)
+            return analyzeExpr((Expr *)y.ptr(), env);
+        return two::analyzeStaticObject(y);
+    }
+
+    case TUPLE : {
+        Tuple *x = (Tuple *)expr.ptr();
+        if ((x->args.size() == 1) &&
+            (x->args[0]->exprKind != VAR_ARGS_REF))
+        {
+            return analyzeExpr(x->args[0], env);
+        }
+        return analyzeCallExpr(primNameRef("tuple"), x->args, env);
+    }
+
+    case ARRAY : {
+        Array *x = (Array *)expr.ptr();
+        return analyzeCallExpr(primNameRef("array"), x->args, env);
+    }
+
+    case INDEXING : {
+        Indexing *x = (Indexing *)expr.ptr();
+        return analyzeIndexingExpr(x->expr, x->args, env);
+    }
+
+    case CALL : {
+        Call *x = (Call *)expr.ptr();
+        return analyzeCallExpr(x->expr, x->args, env);
+    }
+
+    case FIELD_REF : {
+        FieldRef *x = (FieldRef *)expr.ptr();
+        return analyzeFieldRefExpr(x->expr, x->name, env);
+    }
+
+    case TUPLE_REF : {
+        TupleRef *x = (TupleRef *)expr.ptr();
+        PValuePtr pv = two::analyzeOne(x->expr, env);
+        if (!pv)
+            return NULL;
+        MultiPValuePtr args = new MultiPValue(pv);
+        ValueHolderPtr vh = sizeTToValueHolder(x->index);
+        args->values.push_back(staticPValue(vh.ptr()));
+        return analyzeCallValue(kernelPValue("tupleRef"), args);
+    }
+
+    case UNARY_OP : {
+        UnaryOp *x = (UnaryOp *)expr.ptr();
+        if (!x->desugared)
+            x->desugared = desugarUnaryOp(x);
+        return analyzeExpr(x->desugared, env);
+    }
+
+    case BINARY_OP : {
+        BinaryOp *x = (BinaryOp *)expr.ptr();
+        if (!x->desugared)
+            x->desugared = desugarBinaryOp(x);
+        return analyzeExpr(x->desugared, env);
+    }
+
+    case AND : {
+        And *x = (And *)expr.ptr();
+        PValuePtr a = two::analyzeOne(x->expr1, env);
+        if (!a)
+            return NULL;
+        if (a->isTemp)
+            return new MultiPValue(new PValue(a->type, true));
+        PValuePtr b = two::analyzeOne(x->expr2, env);
+        if (!b)
+            return NULL;
+        if (a->type != b->type)
+            error("type mismatch in 'and' expression");
+        PValuePtr pv = new PValue(a->type, a->isTemp || b->isTemp);
+        return new MultiPValue(pv);
+    }
+
+    case OR : {
+        Or *x = (Or *)expr.ptr();
+        PValuePtr a = two::analyzeOne(x->expr1, env);
+        if (!a)
+            return NULL;
+        if (a->isTemp)
+            return new MultiPValue(new PValue(a->type, true));
+        PValuePtr b = two::analyzeOne(x->expr2, env);
+        if (!b)
+            return NULL;
+        if (a->type != b->type)
+            error("type mismatch in 'and' expression");
+        PValuePtr pv = new PValue(a->type, a->isTemp || b->isTemp);
+        return new MultiPValue(pv);
+    }
+
+    case LAMBDA : {
+        Lambda *x = (Lambda *)expr.ptr();
+        if (!x->initialized)
+            initializeLambda(x, env);
+        return analyzeExpr(x->converted, env);
+    }
+
+    case VAR_ARGS_REF : {
+        ObjectPtr y = lookupEnv(env, new Identifier("%varArgs"));
+        VarArgsInfo *vaInfo = (VarArgsInfo *)y.ptr();
+        if (!vaInfo->hasVarArgs)
+            error("varargs unavailable");
+        return analyzeMulti(vaInfo->varArgs, env);
+    }
+
+    case NEW : {
+        New *x = (New *)expr.ptr();
+        if (!x->desugared)
+            x->desugared = desugarNew(x);
+        return analyzeExpr(x->desugared, env);
+    }
+
+    case STATIC_EXPR : {
+        StaticExpr *x = (StaticExpr *)expr.ptr();
+        if (!x->desugared)
+            x->desugared = desugarStaticExpr(x);
+        return analyzeExpr(x->desugared, env);
+    }
+
+    case FOREIGN_EXPR : {
+        ForeignExpr *x = (ForeignExpr *)expr.ptr();
+        return analyzeExpr(x->expr, x->getEnv());
+    }
+
+    case OBJECT_EXPR : {
+        error("ObjectExpr deprecated");
+    }
+
+    default :
+        assert(false);
+        return NULL;
+
+    }
+}
+
+
+
+//
+// analyzeStaticObject
+//
+
+MultiPValuePtr analyzeStaticObject(ObjectPtr x)
+{
+    switch (x->objKind) {
+
+    case ENUM_MEMBER : {
+        EnumMember *y = (EnumMember *)x.ptr();
+        PValuePtr pv = new PValue(y->type, true);
+        return new MultiPValue(pv);
+    }
+
+    case GLOBAL_VARIABLE : {
+        GlobalVariable *y = (GlobalVariable *)x.ptr();
+        PValuePtr pv = analyzeGlobalVariable(y);
+        return new MultiPValue(pv);
+    }
+
+    case EXTERNAL_VARIABLE : {
+        ExternalVariable *y = (ExternalVariable *)x.ptr();
+        PValuePtr pv = analyzeExternalVariable(y);
+        return new MultiPValue(pv);
+    }
+
+    case EXTERNAL_PROCEDURE : {
+        ExternalProcedure *y = (ExternalProcedure *)x.ptr();
+        analyzeExternalProcedure(y);
+        PValuePtr pv = new PValue(y->ptrType, true);
+        return new MultiPValue(pv);
+    }
+
+    case STATIC_GLOBAL : {
+        StaticGlobal *y = (StaticGlobal *)x.ptr();
+        return analyzeExpr(y->expr, y->env);
+    }
+
+    case EVALUE : {
+        EValue *y = (EValue *)x.ptr();
+        PValuePtr pv = new PValue(y->type, false);
+        return new MultiPValue(pv);
+    }
+
+    case MULTI_EVALUE : {
+        MultiEValue *y = (MultiEValue *)x.ptr();
+        MultiPValuePtr z = new MultiPValue();
+        for (unsigned i = 0; i < y->values.size(); ++i)
+            z->values.push_back(new PValue(y->values[i]->type, false));
+        return z;
+    }
+
+    case CVALUE : {
+        CValue *y = (CValue *)x.ptr();
+        PValuePtr pv = new PValue(y->type, false);
+        return new MultiPValue(pv);
+    }
+
+    case MULTI_CVALUE : {
+        MultiCValue *y = (MultiCValue *)x.ptr();
+        MultiPValuePtr z = new MultiPValue();
+        for (unsigned i = 0; i < y->values.size(); ++i)
+            z->values.push_back(new PValue(y->values[i]->type, false));
+        return z;
+    }
+
+    case PVALUE : {
+        PValue *y = (PValue *)x.ptr();
+        return new MultiPValue(y);
+    }
+
+    case MULTI_PVALUE : {
+        MultiPValue *y = (MultiPValue *)x.ptr();
+        return y;
+    }
+
+    case VALUE_HOLDER : {
+        ValueHolder *y = (ValueHolder *)x.ptr();
+        PValuePtr pv = new PValue(y->type, true);
+        return new MultiPValue(pv);
+    }
+
+    case TYPE :
+    case PRIM_OP :
+    case PROCEDURE :
+    case RECORD :
+    case MODULE_HOLDER :
+    case IDENTIFIER : {
+        TypePtr t = staticType(x);
+        PValuePtr pv = new PValue(t, true);
+        return new MultiPValue(pv);
+    }
+
+    case PATTERN :
+        error("pattern cannot be used as value");
+        return NULL;
+
+    default :
+        error("invalid static object");
+        return NULL;
+
+    }
+}
+
+
+
+//
+// analyzeGlobalVariable
+//
+
+PValuePtr analyzeGlobalVariable(GlobalVariablePtr x)
+{
+    if (!x->type) {
+        if (x->analyzing)
+            return NULL;
+        x->analyzing = true;
+        PValuePtr pv = two::analyzeOne(x->expr, x->env);
+        x->analyzing = false;
+        if (!pv)
+            return NULL;
+        x->type = pv->type;
+    }
+    return new PValue(x->type, false);
+}
+
+
+
+//
+// analyzeExternalVariable
+//
+
+PValuePtr analyzeExternalVariable(ExternalVariablePtr x)
+{
+    if (!x->type2)
+        x->type2 = evaluateType(x->type, x->env);
+    return new PValue(x->type2, false);
+}
+
+
+
+//
+// analyzeExternalProcedure
+//
+
+void analyzeExternalProcedure(ExternalProcedurePtr x)
+{
+    if (x->analyzed)
+        return;
+    if (!x->attributesVerified)
+        verifyAttributes(x);
+    vector<TypePtr> argTypes;
+    for (unsigned i = 0; i < x->args.size(); ++i) {
+        ExternalArgPtr y = x->args[i];
+        y->type2 = evaluateType(y->type, x->env);
+        argTypes.push_back(y->type2);
+    }
+    if (!x->returnType)
+        x->returnType2 = NULL;
+    else
+        x->returnType2 = evaluateType(x->returnType, x->env);
+    CallingConv callingConv = CC_DEFAULT;
+    if (x->attrStdCall)
+        callingConv = CC_STDCALL;
+    else if (x->attrFastCall)
+        callingConv = CC_FASTCALL;
+    x->ptrType = cCodePointerType(callingConv, argTypes,
+                                  x->hasVarArgs, x->returnType2);
+    x->analyzed = true;
+}
+
+
+
+//
+// analyzeIndexingExpr
+//
+
+MultiPValuePtr analyzeIndexingExpr(ExprPtr indexable,
+                                   const vector<ExprPtr> &args,
+                                   EnvPtr env)
+{
+    return NULL;
+}
+
+
+
+//
+// analyzeFieldRefExpr
+//
+
+MultiPValuePtr analyzeFieldRefExpr(ExprPtr base,
+                                   IdentifierPtr name,
+                                   EnvPtr env)
+{
+    return NULL;
+}
+
+
+
+//
+// analyzeCallExpr
+//
+
+MultiPValuePtr analyzeCallExpr(ExprPtr callable,
+                               const vector<ExprPtr> &args,
+                               EnvPtr env)
+{
+    return NULL;
+}
+
+
+
+//
+// analyzeCallValue
+//
+
+MultiPValuePtr analyzeCallValue(PValuePtr callable,
+                                MultiPValuePtr args)
+{
+    return NULL;
+}
+
+}
