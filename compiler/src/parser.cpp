2220f7b0c300e413df6b1729c1eb2308d2e05bfc
KS Sreeram <sreeram@tachyontech.net>
2010-09-11 08:56:51 +0530

    removed syntactic support for 'unreachable'. it's only meant for internal use.

--- compiler/src/parser.cpp
+++ compiler/src/parser.cpp
@@ -1378,15 +1378,6 @@ static bool staticFor(StatementPtr &x) {
     return true;
 }
 
-static bool unreachable(StatementPtr &x) {
-    LocationPtr location = currentLocation();
-    if (!keyword("unreachable")) return false;
-    if (!symbol(";")) return false;
-    x = new Unreachable();
-    x->location = location;
-    return true;
-}
-
 static bool statement(StatementPtr &x) {
     int p = save();
     if (block(x)) return true;
@@ -1405,7 +1396,6 @@ static bool statement(StatementPtr &x) {
     if (restore(p), tryStatement(x)) return true;
     if (restore(p), throwStatement(x)) return true;
     if (restore(p), staticFor(x)) return true;
-    if (restore(p), unreachable(x)) return true;
 
     return false;
 }
