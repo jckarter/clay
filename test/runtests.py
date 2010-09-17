d42b52ff8a2acfe615d480ad593ffe134ca92dda
Joe Groff <arcata@gmail.com>
2010-09-17 17:48:55 +0530

    update runtests.py script so that it will verify a test.clay file by checking the return code of the compiled program instead of comparing its output to out.txt

--- test/runtests.py
+++ test/runtests.py
@@ -46,17 +46,16 @@ compiler = getCompilerPath()
 
 class TestCase(object):
     allCases = []
-    def __init__(self, folder, base = None):
+    def __init__(self, folder, testfile, base = None):
         entries = os.listdir(folder)
-        mainPath = os.path.join(folder, "main.clay")
         self.path = folder
-        if os.path.isfile(mainPath):
-            self.loadTest(mainPath)
+        if os.path.isfile(testfile):
+            self.loadTest(testfile)
         for entry in entries:
             fullpath = os.path.join(folder, entry)
             if not os.path.isdir(fullpath):
                 continue
-            TestCase(fullpath, self)
+            findTestCase(fullpath, self)
     
     def loadTest(self, testfile):
         TestCase.allCases.append(self)
@@ -75,7 +74,7 @@ class TestCase(object):
         [os.unlink(f) for f in glob.glob("temp*")]
         [os.unlink(f) for f in glob.glob("*.data")]
 
-    def match(self, output) :
+    def match(self, output, returncode) :
         refoutput = open("out.txt").read()
         output = output.replace('\r', '')
         refoutput = refoutput.replace('\r', '')
@@ -85,9 +84,9 @@ class TestCase(object):
         os.chdir(self.path)
         self.pre_build()
         self.post_build()
-        result = self.runtest()
+        result, returncode = self.runtest()
         self.post_run()
-        if self.match(result):
+        if self.match(result, returncode):
             return "ok"
         return "fail"
 
@@ -99,12 +98,12 @@ class TestCase(object):
         outfilename = os.path.join(".", outfilename)
         process = Popen(self.cmdline(compiler))
         if process.wait() != 0 :
-            return "fail"
+            return "fail", None
         process = Popen([outfilename], stdout=PIPE)
         result = process.stdout.read()
         process.wait()
         self.removefile(outfilename)
-        return result
+        return result, process.returncode
 
     def removefile(self, filename) :
         # on windows, sometimes, deleting a file
@@ -119,23 +118,37 @@ class TestCase(object):
                 time.sleep(1)
             attempts += 1
 
+class TestModuleCase(TestCase):
+    def cmdline(self, clay) :
+        return [clay, "test.clay"]
 
+    def match(self, output, returncode) :
+        return returncode == 0
+        
 
 #
 # runtests
 #
 
+def findTestCase(folder, base = None):
+    testPath = os.path.join(folder, "test.clay")
+    mainPath = os.path.join(folder, "main.clay")
+    if os.path.isfile(testPath) :
+        TestModuleCase(folder, testPath, base)
+    else :
+        TestCase(folder, mainPath, base)
+
 def findTestCases():
-    TestCase(testRoot)
+    findTestCase(testRoot)
     return TestCase.allCases
 
-def runtest(t):
+def runTest(t):
     return t.run()
 
-def runtests() :
+def runTests() :
     testcases = findTestCases()
     pool = Pool(processes = cpu_count())
-    results = pool.imap(runtest, testcases)
+    results = pool.imap(runTest, testcases)
     failed = []
     for test in testcases:
         res = results.next()
@@ -151,7 +164,7 @@ def runtests() :
 
 if __name__ == "__main__":
     startTime = time.time()
-    runtests()
+    runTests()
     endTime = time.time()
     print ""
     print "time taken = %f seconds" % (endTime - startTime)
