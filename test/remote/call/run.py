bcfc4e039e2e23a6ab481abec3642a8646c3cb46
Joe Groff <arcata@gmail.com>
2010-09-18 07:50:10 +0530

    remote.call: update example to be testable using custom run.py script
    --HG--
    rename : test/remote/call/example.clay => test/remote/call/main.clay

---
+++ test/remote/call/run.py
@@ -0,0 +1,13 @@
+from subprocess import Popen, PIPE
+from sys import argv
+
+server = Popen([argv[1], "server"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+client = Popen([argv[1], "client", "zim", "zang", "zung"], stdin=server.stdout, stdout=server.stdin, stderr=PIPE)
+
+servererr = server.communicate()[1]
+clienterr = client.communicate()[1]
+
+print "-- server stderr"
+print servererr,
+print "-- client stderr"
+print clienterr,
