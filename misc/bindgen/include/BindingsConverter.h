e806256fde9c998b39e3eb5f865f4cc77cf81688
Joe Groff <arcata@gmail.com>
2010-10-18 16:30:16 +0530

    bindgen: generate bindings for objective-c classes and selectors using cocoa.objc

--- misc/bindgen/include/BindingsConverter.h
+++ misc/bindgen/include/BindingsConverter.h
@@ -13,6 +13,7 @@
 using namespace std;
 
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/DeclObjC.h"
 #include "clang/Basic/Diagnostic.h"
 #include "clang/Frontend/TextDiagnosticPrinter.h"
 #include "clang/Frontend/DiagnosticOptions.h"
@@ -34,10 +35,14 @@ private:
     string convertBuiltinType(const BuiltinType *type);
     string convertFPType(const FunctionNoProtoType *type);
     string convertFPType(const FunctionProtoType *type);
+    string convertObjcType(const Type *type);
     string convertType(const Type *type);
     string convertIdent(const string &cIdent);
 
     bool isClayKeyword(const string &cIdent);
+
+    void allocateObjcMethodSelector(ObjCMethodDecl *method);
+    void allocateObjcPropertySelector(ObjCPropertyDecl *property);
 
 public :
     virtual void Initialize(ASTContext &astc);
@@ -49,9 +54,11 @@ public :
 private :
     void generateDecl(Decl *decl);
     void generateHeader();
+    void generateObjC();
 
     bool declMatches(Decl *decl);
 
+    bool combineObjcType(QualType *inout_a, QualType b);
 private :
     ostream& out;
     vector<Decl*> decls;
@@ -62,6 +69,28 @@ private :
     vector<RecordDecl*> pendingAnonRecords;
     map<string, string> recordNames;
     map<string, string> typedefNames;
+
+    map<string, ObjCInterfaceDecl*> objcClassNames;
+
+    struct selector_info {
+        QualType resultType;
+        bool isVariadic;
+        vector<QualType> paramTypes;
+
+        selector_info() {}
+
+        selector_info(ObjCMethodDecl *decl)
+            : resultType(decl->getResultType()),
+              isVariadic(decl->isVariadic()) {
+            for (ObjCMethodDecl::param_iterator i = decl->param_begin();
+                 i != decl->param_end();
+                 ++i) {
+                paramTypes.push_back((*i)->getType());
+            }
+        }
+    };
+
+    map<string, selector_info> objcSelectors;
 
     set<string> externsDeclared;
 
