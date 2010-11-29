db7b59cf3ae80d8bb58c2c7e4ee5f310f48dc833
Joe Groff <arcata@gmail.com>
2010-11-29 14:58:48 +0530

    test program for struct returns

---
+++ test/codepointers/structreturn/foo.c
@@ -0,0 +1,17 @@
+typedef struct Struct4 { int x; } Struct4;
+typedef struct Struct8 { Struct4 x; Struct4 y; } Struct8;
+typedef struct Struct16 { Struct8 x; Struct8 y; } Struct16;
+typedef struct Struct32 { Struct16 x; Struct16 y; } Struct32;
+typedef struct Struct64 { Struct32 x; Struct32 y; } Struct64;
+
+Struct4  foo4 (int x)                  { return (Struct4 ){x};    }
+Struct8  foo8 (Struct4  x, Struct4  y) { return (Struct8 ){x, y}; }
+Struct16 foo16(Struct8  x, Struct8  y) { return (Struct16){x, y}; }
+Struct32 foo32(Struct16 x, Struct16 y) { return (Struct32){x, y}; }
+Struct64 foo64(Struct32 x, Struct32 y) { return (Struct64){x, y}; }
+
+Struct4  bar4 (Struct4  *r, int x)                  { *r = (Struct4 ){x};    }
+Struct8  bar8 (Struct8  *r, Struct4  x, Struct4  y) { *r = (Struct8 ){x, y}; }
+Struct16 bar16(Struct16 *r, Struct8  x, Struct8  y) { *r = (Struct16){x, y}; }
+Struct32 bar32(Struct32 *r, Struct16 x, Struct16 y) { *r = (Struct32){x, y}; }
+Struct64 bar64(Struct64 *r, Struct32 x, Struct32 y) { *r = (Struct64){x, y}; }
