cda3613469bd51019001d03ffec5906a7767f894
KS Sreeram <sreeram@tachyontech.net>
2010-12-13 14:55:14 +0530

    test/codepointers/structreturn: fix return types in foo.c.

--- test/codepointers/structreturn/foo.c
+++ test/codepointers/structreturn/foo.c
@@ -10,8 +10,8 @@ Struct16 foo16(Struct8  x, Struct8  y) { return (Struct16){x, y}; }
 Struct32 foo32(Struct16 x, Struct16 y) { return (Struct32){x, y}; }
 Struct64 foo64(Struct32 x, Struct32 y) { return (Struct64){x, y}; }
 
-Struct4  bar4 (Struct4  *r, int x)                  { *r = (Struct4 ){x};    }
-Struct8  bar8 (Struct8  *r, Struct4  x, Struct4  y) { *r = (Struct8 ){x, y}; }
-Struct16 bar16(Struct16 *r, Struct8  x, Struct8  y) { *r = (Struct16){x, y}; }
-Struct32 bar32(Struct32 *r, Struct16 x, Struct16 y) { *r = (Struct32){x, y}; }
-Struct64 bar64(Struct64 *r, Struct32 x, Struct32 y) { *r = (Struct64){x, y}; }
+void bar4 (Struct4  *r, int x)                  { *r = (Struct4 ){x};    }
+void bar8 (Struct8  *r, Struct4  x, Struct4  y) { *r = (Struct8 ){x, y}; }
+void bar16(Struct16 *r, Struct8  x, Struct8  y) { *r = (Struct16){x, y}; }
+void bar32(Struct32 *r, Struct16 x, Struct16 y) { *r = (Struct32){x, y}; }
+void bar64(Struct64 *r, Struct32 x, Struct32 y) { *r = (Struct64){x, y}; }
