bc4a06a2333dff45f92ff544b3af07eae548d185
Joe Groff <arcata@gmail.com>
2010-12-23 12:22:44 +0530

    newclay: streamline test.sh script
    --HG--
    rename : newclay/test/testevaluator/testevaluator.sh => newclay/test.sh

---
+++ newclay/test.sh
@@ -0,0 +1,13 @@
+if [ x$1 = x ]; then
+    echo "usage: $0 testwhatever"
+    exit 1
+fi
+
+make $1
+for test in test/$1/Fail*.clay test/$1/Test*.clay; do
+    echo
+    echo --------------------
+    echo $test
+    echo --------------------
+    ./$1 $test
+done
--- newclay/test/testloader/testloader.sh
+++
@@ -1,7 +0,0 @@
-for test in Fail*.clay Test*.clay; do
-    echo
-    echo --------------------
-    echo $test
-    echo --------------------
-    ../../testloader $test
-done
