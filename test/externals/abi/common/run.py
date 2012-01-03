349bc738d3d0bda531d6bdf0b3f50464e2ec9975
Joe Groff <arcata@gmail.com>
2012-01-03 14:48:28 -0800

    fix externals/abi/common test for msvc

--- test/externals/abi/common/run.py
+++ test/externals/abi/common/run.py
@@ -9,11 +9,12 @@ linkFlags = [];
 if platform == 'linux' or platform == 'linux2':
     linkFlags += ['-lm']
 
+NULL = open(os.devnull, 'w')
+
 try:
     if platform == 'win32':
         os.rename(clayobj, "temp-main.obj")
-        check_call(["cl", "/nologo", "/Fetemp.exe", "/Fotemp-external_test.obj", "external_test.cpp", "temp-main.obj"])
-
+        check_call(["cl", "/nologo", "/Fetemp.exe", "/Fotemp-external_test.obj", "external_test.cpp", "temp-main.obj"], stdout=NULL)
     else:
         os.rename(clayobj, "temp-main.o")
         check_call(["clang++", "-c", "-o", "temp-external_test.o", "external_test.cpp"] + buildFlags)
