783d022cbfc6cf3e1e2d144ca8fcfd9f715334df
Jeremy Clifford <jclifford77@gmail.com>
2012-04-11 04:18:52 +0100

    Breakout libm bindings from libc bindings, move to math/libm. Move some functions out of numbers/floats to math/native.

--- lib-clay/libc/generated/generated_from_unix.h
+++ lib-clay/libc/generated/generated_from_unix.h
@@ -2,7 +2,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <complex.h>
-#include <math.h>
 #include <setjmp.h>
 #include <time.h>
---
+++ lib-clay/math/libm/generated/generated_from_unix.h
@@ -0,0 +1,2 @@
+#include <complex.h>
+#include <math.h>
