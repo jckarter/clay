fd84a63e6d91fe9abbc1098f04b1cab4d281c891
Jeremy Clifford <jclifford77@gmail.com>
2012-04-12 14:18:03 +0100

    Add core/libc

---
+++ lib-clay/core/libc/generated/generated_from_unix.h
@@ -0,0 +1,6 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <setjmp.h>
+#include <time.h>
---
+++ lib-clay/core/libc/generated/generated_from_win32.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <setjmp.h>
+#include <time.h>
