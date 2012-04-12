712eff023235f7c06a6db69f74974a4cb238bef7
Jeremy Clifford <jclifford77@gmail.com>
2012-04-12 14:16:23 +0100

    Use explicit references to libc.<fn> rather than import

--- lib-clay/libc/generated/generated_from_unix.h
+++
@@ -1,6 +0,0 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <setjmp.h>
-#include <time.h>
--- lib-clay/libc/generated/generated_from_win32.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <io.h>
-#include <setjmp.h>
-#include <time.h>
