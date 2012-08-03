c5d6a40f47bf88399fbfc7ee6f209fc020588efa
Jeremy Clifford <jclifford77@gmail.com>
2012-08-03 11:46:18 +0100

    replace swtich in codegenbinding

--- compiler/src/codegen.cpp
+++ compiler/src/codegen.cpp
@@ -2452,7 +2452,7 @@ void codegenCallCode(InvokeEntry* entry,
                      CodegenContext* ctx,
                      MultiCValuePtr out)
 {
-    if (inlineEnabled() && entry->isInline==FORCE_INLINE && !entry->code->isLLVMBody()) {
+    if (inlineEnabled() && entry->isInline && !entry->code->isLLVMBody()) {
         codegenCallInline(entry, args, ctx, out);
         return;
     }
@@ -2713,7 +2713,7 @@ void codegenLLVMBody(InvokeEntry* entry, llvm::StringRef callableName)
     }
 
     out << ") ";
-    if (entry->isInline==FORCE_INLINE)
+    if (entry->isInline)
         out << "alwaysinline ";
 
     string body;
@@ -2847,18 +2847,6 @@ void codegenCodeBody(InvokeEntry* entry)
                                llvmFuncName,
                                llvmModule);
 
-    switch(entry->isInline){
-    case INLINE : 
-        if(inlineEnabled())
-            llFunc->addFnAttr(llvm::Attribute::InlineHint);
-        break;
-    case NEVER_INLINE :
-        llFunc->addFnAttr(llvm::Attribute::NoInline);
-        break;
-    default:
-        break;
-    }
-    
     for (unsigned i = 1; i <= llArgTypes.size(); ++i) {
         llFunc->addAttribute(i, llvm::Attribute::NoAlias);
     }
@@ -3231,7 +3219,7 @@ void codegenCallInline(InvokeEntry* entry,
                        CodegenContext* ctx,
                        MultiCValuePtr out)
 {
-    assert(entry->isInline==FORCE_INLINE);
+    assert(entry->isInline);
     assert(ctx->inlineDepth >= 0);
     if (entry->code->isLLVMBody())
         error(entry->code, "llvm procedures cannot be inlined");
@@ -4068,7 +4056,7 @@ EnvPtr codegenBinding(BindingPtr x, EnvPtr env, CodegenContext* ctx)
 
     int line, column;
     llvm::DIFile file = getDebugLineCol(x->location, line, column);
-
+    switch(x->bindingKind){
     case VAR : {
         llvm::errs() << "codegenBinding:VAR\n";
         
