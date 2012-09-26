36a016cb78804c3fcb5e14bc689550a61a550f08
Dmitry Galchinsky <galchinsky@gmail.com>
2012-09-27 03:20:56 +0400

    add exception handling to llvm and clay exceptions

--- compiler/src/interactive.cpp
+++ compiler/src/interactive.cpp
@@ -2,69 +2,87 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <signal.h>
 
 namespace clay {
 
-using std::ostringstream;
+    using std::ostringstream;
 
-const char* replAnonymousFunctionName = "__replAnonymousFunction__";
+    const char* replAnonymousFunctionName = "__replAnonymousFunction__";
 
-void runInteractive(llvm::Module *llvmModule, ModulePtr module)
-{
-    std::cout << "Clay interpreter\n";
-    std::cout << ":q to exit\n";
+    static ModulePtr module;
+    static llvm::ExecutionEngine *engine;
 
-    setExitOnError(false);
-    llvm::EngineBuilder eb(llvmModule);
-    llvm::ExecutionEngine *engine = eb.create();
-    engine->runStaticConstructorsDestructors(false);
-    string line;
-    int lineNum = 0;
-    while(true) {
-        std::cout.flush();
-        std::cout << "clay>";
-        getline (std::cin, line);
-        if (line == ":q")
-            break;
+    static void interactiveLoop()
+    {
+        string line;
+        static int lineNum = 0;
+        while(true) {
+            std::cout.flush();
+            std::cout << "clay>";
+            getline (std::cin, line);
+            if (line == ":q")
+                break;
 
-        SourcePtr source = new Source(line, 0);
-        vector<StatementPtr> statements;    
-        try {
-            parseInteractive(source, 0, source->size(), statements);
+            SourcePtr source = new Source(line, 0);
+            vector<StatementPtr> statements;
+            try {
+                parseInteractive(source, 0, source->size(), statements);
+            }
+            catch (std::exception) {
+                continue;
+            }
+            ostringstream funName;
+            funName << replAnonymousFunctionName << lineNum;
+            IdentifierPtr fun = Identifier::get(funName.str());
+            ++lineNum ;
+
+            BlockPtr funBody = new Block(statements);
+            ExternalProcedurePtr entryProc =
+                    new ExternalProcedure(fun,
+                                          PRIVATE,
+                                          vector<ExternalArgPtr>(),
+                                          false,
+                                          NULL,
+                                          funBody.ptr(),
+                                          new ExprList());
+
+            entryProc->env = module->env;
+            try {
+                codegenExternalProcedure(entryProc, true);
+            }
+            catch (std::exception) {
+                continue;
+            }
+
+            engine->runFunction(entryProc->llvmFunc, std::vector<llvm::GenericValue>());
         }
-        catch (std::exception) {
-            continue;
-        }
-        ostringstream funName;
-        funName << replAnonymousFunctionName << lineNum;
-        IdentifierPtr fun = Identifier::get(funName.str());
-        ++lineNum ;
-
-        BlockPtr funBody = new Block(statements);
-        ExternalProcedurePtr entryProc =
-                new ExternalProcedure(fun,
-                                    PRIVATE,
-                                    vector<ExternalArgPtr>(),
-                                    false,
-                                    NULL,
-                                    funBody.ptr(),
-                                    new ExprList());
-
-        entryProc->env = module->env;
-        try {
-            codegenExternalProcedure(entryProc, true);
-        }
-        catch (std::exception) {
-            continue;
-        }
-
-        void* fPtr = engine->getPointerToFunction(entryProc->llvmFunc);
-        void (*f)() = (void (*)())fPtr;
-
-        f();
-
+        engine->runStaticConstructorsDestructors(true);
     }
-    engine->runStaticConstructorsDestructors(true);
-}
+
+    static void exceptionHandler(int i)
+    {
+        sigrelse(SIGABRT);
+        interactiveLoop();
+    }
+
+    void runInteractive(llvm::Module *llvmModule_, ModulePtr module_)
+    {
+
+        signal(SIGABRT, exceptionHandler);
+
+        llvmModule = llvmModule_;
+        module = module_;
+
+        std::cout << "Clay interpreter\n";
+        std::cout << ":q to exit\n";
+
+        setExitOnError(false);
+        llvm::EngineBuilder eb(llvmModule);
+        engine = eb.create();
+        engine->runStaticConstructorsDestructors(false);
+
+        interactiveLoop();
+    }
 
 }
