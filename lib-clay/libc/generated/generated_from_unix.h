1ebe596f198e23f53eb00f31fc9ca154bc7f465f
Dmitry Galchinsky <galchinsky@gmail.com>
2012-10-06 13:53:44 +0400

    fixes after merging 3

---
+++ lib-clay/libc/generated/generated_from_unix.h
@@ -0,0 +1,8 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <complex.h>
+#include <math.h>
+#include <setjmp.h>
+#include <time.h>
---
+++ lib-clay/libc/generated/generated_from_win32.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <math.h>
+#include <setjmp.h>
+#include <time.h>
