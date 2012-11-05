jckarter/clay

commit 4ed588f07aaefd487adaa55dc2552bad88411f83

more space-efficient memory layouts in compiler

compiler/clay.hpp
@@ -81,6 +81,16 @@
 
 namespace clay {
 
+template<bool Cond> struct StaticAssertChecker;
+template<> struct StaticAssertChecker<true> { typedef int type; };
+template<> struct StaticAssertChecker<false> { };
+
+#define _CLAY_CAT(a,b) a##b
+#define CLAY_CAT(a,b) _CLAY_CAT(a,b)
+
+#define CLAY_STATIC_ASSERT(cond) \
+    static const ::clay::StaticAssertChecker<(cond)>::type CLAY_CAT(static_assert_, __LINE__);
+
 using std::string;
 using std::vector;
 using std::pair;
@@ -754,6 +764,7 @@ struct Source : public Object {
     string fileName;
     llvm::OwningPtr<llvm::MemoryBuffer> buffer;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
+
     Source(llvm::StringRef fileName)
         : Object(SOURCE), fileName(fileName), debugInfo(NULL)
     {
@@ -797,12 +808,14 @@ struct Location {
 //
 
 struct CompileContextEntry {
-    ObjectPtr callable;
-    bool hasParams;
     vector<ObjectPtr> params;
     vector<unsigned> dispatchIndices;
 
     Location location;
+
+    ObjectPtr callable;
+
+    bool hasParams:1;
 
     CompileContextEntry(ObjectPtr callable)
         : callable(callable), hasParams(false) {}
@@ -1108,8 +1121,8 @@ struct FloatLiteral : public Expr {
 };
 
 struct CharLiteral : public Expr {
-    char value;
     ExprPtr desugared;
+    char value;
     CharLiteral(char value)
         : Expr(CHAR_LITERAL), value(value) {}
 };
@@ -1249,13 +1262,11 @@ enum LambdaCapture {
 struct Lambda : public Expr {
     LambdaCapture captureBy;
     vector<FormalArgPtr> formalArgs;
-    bool hasVarArg;
 
     StatementPtr body;
 
     ExprPtr converted;
 
-    bool initialized;
     vector<string> freeVars;
 
     ProcedureMono mono;
@@ -1267,6 +1278,9 @@ struct Lambda : public Expr {
     // if freevars are absent
     ProcedurePtr lambdaProc;
 
+    bool hasVarArg:1;
+    bool initialized:1;
+
     Lambda(LambdaCapture captureBy) :
         Expr(LAMBDA), captureBy(captureBy),
         hasVarArg(false), initialized(false) {}
@@ -1274,8 +1288,8 @@ struct Lambda : public Expr {
            llvm::ArrayRef<FormalArgPtr> formalArgs,
            bool hasVarArg, StatementPtr body)
         : Expr(LAMBDA), captureBy(captureBy),
-          formalArgs(formalArgs), hasVarArg(hasVarArg),
-          body(body), initialized(false) {}
+          formalArgs(formalArgs), body(body),
+          hasVarArg(hasVarArg), initialized(false) {}
 };
 
 struct Unpack : public Expr {
@@ -1321,8 +1335,8 @@ struct ObjectExpr : public Expr {
 
 struct EvalExpr : public Expr {
     ExprPtr args;
-    bool evaled;
     ExprListPtr value;
+    bool evaled;
 
     EvalExpr(ExprPtr args)
         : Expr(EVAL_EXPR), args(args), evaled(false) {}
@@ -1476,11 +1490,11 @@ struct InitAssignment : public Statement {
 };
 
 struct VariadicAssignment : public Statement {
-    int op;
     ExprListPtr exprs;
     ExprPtr desugared;
+    int op;
     VariadicAssignment(int op, ExprListPtr exprs )
-        : Statement(VARIADIC_ASSIGNMENT), op(op), exprs(exprs) {}
+        : Statement(VARIADIC_ASSIGNMENT), exprs(exprs), op(op) {}
 };
 
 struct Goto : public Statement {
@@ -1496,16 +1510,16 @@ enum ReturnKind {
 };
 
 struct Return : public Statement {
-    ReturnKind returnKind;
     ExprListPtr values;
-    bool isExprReturn;
-    bool isReturnSpecs;
+    ReturnKind returnKind:2;
+    bool isExprReturn:1;
+    bool isReturnSpecs:1;
     Return(ReturnKind returnKind, ExprListPtr values)
-        : Statement(RETURN), returnKind(returnKind), values(values), 
-          isExprReturn(false), isReturnSpecs(false) {}
+        : Statement(RETURN), values(values),
+          returnKind(returnKind), isExprReturn(false), isReturnSpecs(false) {}
     Return(ReturnKind returnKind, ExprListPtr values, bool exprRet)
-        : Statement(RETURN), returnKind(returnKind), values(values), 
-          isExprReturn(exprRet), isReturnSpecs(false) {}
+        : Statement(RETURN), values(values), 
+          returnKind(returnKind), isExprReturn(exprRet), isReturnSpecs(false) {}
 };
 
 struct If : public Statement {
@@ -1683,8 +1697,8 @@ struct Unreachable : public Statement {
 
 struct EvalStatement : public Statement {
     ExprListPtr args;
-    bool evaled;
     vector<StatementPtr> value;
+    bool evaled:1;
 
     EvalStatement(ExprListPtr args)
         : Statement(EVAL_STATEMENT), args(args), evaled(false) {}
@@ -1724,7 +1738,7 @@ struct FormalArg : public ANode {
     IdentifierPtr name;
     ExprPtr type;
     ValueTempness tempness;
-    bool varArg;
+    bool varArg:1;
     FormalArg(IdentifierPtr name, ExprPtr type)
         : ANode(FORMAL_ARG), name(name), type(type),
           tempness(TEMPNESS_DONTCARE), varArg(false) {}
@@ -1746,10 +1760,10 @@ struct ReturnSpec : public ANode {
 };
 
 struct PatternVar {
-    bool isMulti;
     IdentifierPtr name;
+    bool isMulti:1;
     PatternVar(bool isMulti, IdentifierPtr name)
-        : isMulti(isMulti), name(name) {}
+        : name(name), isMulti(isMulti) {}
     PatternVar()
         : isMulti(false) {}
 };
@@ -1764,12 +1778,12 @@ struct Code : public ANode {
     vector<PatternVar> patternVars;
     ExprPtr predicate;
     vector<FormalArgPtr> formalArgs;
-    bool hasVarArg;
-    bool returnSpecsDeclared;
     vector<ReturnSpecPtr> returnSpecs;
     ReturnSpecPtr varReturnSpec;
     StatementPtr body;
     LLVMCodePtr llvmBody;
+    bool hasVarArg:1;
+    bool returnSpecsDeclared:1;
 
     Code()
         : ANode(CODE),  hasVarArg(false), returnSpecsDeclared(false) {}
@@ -1780,9 +1794,11 @@ struct Code : public ANode {
          ReturnSpecPtr varReturnSpec,
          StatementPtr body)
         : ANode(CODE), patternVars(patternVars), predicate(predicate),
-          formalArgs(formalArgs), hasVarArg(false), returnSpecsDeclared(false),
+          formalArgs(formalArgs),
           returnSpecs(returnSpecs), varReturnSpec(varReturnSpec),
-          body(body) {}
+          body(body),
+          hasVarArg(false), returnSpecsDeclared(false)
+          {}
 
     bool hasReturnSpecs() {
         return returnSpecsDeclared;
@@ -1843,9 +1859,10 @@ struct RecordDecl : public TopLevelItem {
     RecordBodyPtr body;
 
     vector<OverloadPtr> overloads;
-    bool builtinOverloadInitialized;
 
     LambdaPtr lambda;
+
+    bool builtinOverloadInitialized:1;
 
     RecordDecl(Module *module, Visibility visibility)
         : TopLevelItem(RECORD_DECL, module, visibility),
@@ -1870,13 +1887,13 @@ struct RecordDecl : public TopLevelItem {
 };
 
 struct RecordBody : public ANode {
-    bool isComputed;
     ExprListPtr computed; // valid if isComputed == true
     vector<RecordFieldPtr> fields; // valid if isComputed == false
+    bool isComputed:1;
     RecordBody(ExprListPtr computed)
-        : ANode(RECORD_BODY), isComputed(true), computed(computed) {}
+        : ANode(RECORD_BODY), computed(computed), isComputed(true) {}
     RecordBody(llvm::ArrayRef<RecordFieldPtr> fields)
-        : ANode(RECORD_BODY), isComputed(false), fields(fields) {}
+        : ANode(RECORD_BODY), fields(fields), isComputed(false) {}
 };
 
 struct RecordField : public ANode {
@@ -1893,12 +1910,13 @@ struct VariantDecl : public TopLevelItem {
     vector<IdentifierPtr> params;
     IdentifierPtr varParam;
 
-    bool open;
     ExprListPtr defaultInstances;
 
     vector<InstanceDeclPtr> instances;
 
     vector<OverloadPtr> overloads;
+
+    bool open:1;
 
     VariantDecl(Module *module, IdentifierPtr name,
             Visibility visibility,
@@ -1911,7 +1929,8 @@ struct VariantDecl : public TopLevelItem {
         : TopLevelItem(VARIANT_DECL, module, name, visibility),
           patternVars(patternVars), predicate(predicate),
           params(params), varParam(varParam),
-          open(open), defaultInstances(defaultInstances) {}
+          defaultInstances(defaultInstances),
+          open(open) {}
 };
 
 struct InstanceDecl : public TopLevelItem {
@@ -1939,29 +1958,28 @@ enum InlineAttribute {
 struct Overload : public TopLevelItem {
     ExprPtr target;
     CodePtr code;
-    bool callByName;
-    InlineAttribute isInline;
 
     // pre-computed patterns for matchInvoke
-    int patternsInitializedState; // 0:notinit, -1:initing, +1:inited
-    bool nameIsPattern;
     vector<PatternCellPtr> cells;
     vector<MultiPatternCellPtr> multiCells;
     PatternPtr callablePattern;
     vector<PatternPtr> argPatterns;
     MultiPatternPtr varArgPattern;
+    InlineAttribute isInline:2;
+    int patternsInitializedState:2; // 0:notinit, -1:initing, +1:inited
+    bool callByName:1;
+    bool nameIsPattern:1;
     
     Overload(Module *module, ExprPtr target,
              CodePtr code,
              bool callByName,
              InlineAttribute isInline)
         : TopLevelItem(OVERLOAD, module), target(target), code(code),
-          callByName(callByName), isInline(isInline),
-          patternsInitializedState(0), nameIsPattern(false) {}
+          isInline(isInline), patternsInitializedState(0),
+          callByName(callByName), nameIsPattern(false) {}
 };
 
 struct Procedure : public TopLevelItem {
-    bool privateOverload;
     OverloadPtr interface;
 
     OverloadPtr singleOverload;
@@ -1970,6 +1988,8 @@ struct Procedure : public TopLevelItem {
     ProcedureMono mono;
     LambdaPtr lambda;
 
+    bool privateOverload:1;
+
     Procedure(Module *module, IdentifierPtr name, Visibility visibility, bool privateOverload)
         : TopLevelItem(PROCEDURE, module, name, visibility),
           privateOverload(privateOverload)
@@ -1977,7 +1997,7 @@ struct Procedure : public TopLevelItem {
 
     Procedure(Module *module, IdentifierPtr name, Visibility visibility, bool privateOverload, OverloadPtr interface)
         : TopLevelItem(PROCEDURE, module, name, visibility),
-          privateOverload(privateOverload), interface(interface)
+          interface(interface), privateOverload(privateOverload)
     {}
 };
 
@@ -1999,7 +2019,7 @@ struct NewTypeDecl : public TopLevelItem {
     ExprPtr expr;
     TypePtr type;
     TypePtr baseType;
-    bool initialized;
+    bool initialized:1;
     NewTypeDecl(Module *module, IdentifierPtr name,
             Visibility visibility,
             ExprPtr expr)
@@ -2029,8 +2049,8 @@ struct EnumDecl : public TopLevelItem {
 
 struct EnumMember : public ANode {
     IdentifierPtr name;
-    int index;
     TypePtr type;
+    int index;
     EnumMember(IdentifierPtr name)
         : ANode(ENUM_MEMBER), name(name), index(-1) {}
 };
@@ -2065,7 +2085,6 @@ struct GVarInstance : public Object {
     GlobalVariablePtr gvar;
     vector<ObjectPtr> params;
 
-    bool analyzing;
     ExprPtr expr;
     EnvPtr env;
     MultiPValuePtr analysis;
@@ -2074,10 +2093,12 @@ struct GVarInstance : public Object {
     llvm::GlobalVariable *llGlobal;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
 
+    bool analyzing:1;
+
     GVarInstance(GlobalVariablePtr gvar,
                  llvm::ArrayRef<ObjectPtr> params)
         : Object(DONT_CARE), gvar(gvar), params(params),
-          analyzing(false), llGlobal(NULL), debugInfo(NULL) {}
+          llGlobal(NULL), debugInfo(NULL), analyzing(false) {}
 
     llvm::DIGlobalVariable getDebugInfo() { return llvm::DIGlobalVariable(debugInfo); }
 };
@@ -2093,29 +2114,34 @@ enum CallingConv {
 
 struct ExternalProcedure : public TopLevelItem {
     vector<ExternalArgPtr> args;
-    bool hasVarArgs;
     ExprPtr returnType;
     StatementPtr body;
     ExprListPtr attributes;
 
-    bool attributesVerified;
-    bool attrDLLImport;
-    bool attrDLLExport;
-    CallingConv callingConv;
     llvm::SmallString<16> attrAsmLabel;
 
-    bool analyzed;
-    bool bodyCodegenned;
     TypePtr returnType2;
     TypePtr ptrType;
 
     llvm::Function *llvmFunc;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
 
+    CallingConv callingConv:3;
+    bool hasVarArgs:1;
+    bool attributesVerified:1;
+    bool attrDLLImport:1;
+    bool attrDLLExport:1;
+    bool analyzed:1;
+    bool bodyCodegenned:1;
+
     ExternalProcedure(Module *module, Visibility visibility)
-        : TopLevelItem(EXTERNAL_PROCEDURE, module, visibility), hasVarArgs(false),
-          attributes(new ExprList()), attributesVerified(false),
-          analyzed(false), bodyCodegenned(false), llvmFunc(NULL), debugInfo(NULL) {}
+        : TopLevelItem(EXTERNAL_PROCEDURE, module, visibility),
+          attributes(new ExprList()),
+          llvmFunc(NULL), debugInfo(NULL),
+          hasVarArgs(false),
+          attributesVerified(false),
+          analyzed(false), bodyCodegenned(false)
+        {}
     ExternalProcedure(Module *module, IdentifierPtr name,
                       Visibility visibility,
                       llvm::ArrayRef<ExternalArgPtr> args,
@@ -2124,9 +2150,13 @@ struct ExternalProcedure : public TopLevelItem {
                       StatementPtr body,
                       ExprListPtr attributes)
         : TopLevelItem(EXTERNAL_PROCEDURE, module, name, visibility), args(args),
-          hasVarArgs(hasVarArgs), returnType(returnType), body(body),
-          attributes(attributes), attributesVerified(false),
-          analyzed(false), bodyCodegenned(false), llvmFunc(NULL), debugInfo(NULL) {}
+          returnType(returnType), body(body),
+          attributes(attributes),
+          llvmFunc(NULL), debugInfo(NULL),
+          hasVarArgs(hasVarArgs),
+          attributesVerified(false),
+          analyzed(false), bodyCodegenned(false)
+        {}
 
     llvm::DISubprogram getDebugInfo() { return llvm::DISubprogram(debugInfo); }
 };
@@ -2143,33 +2173,37 @@ struct ExternalVariable : public TopLevelItem {
     ExprPtr type;
     ExprListPtr attributes;
 
-    bool attributesVerified;
-    bool attrDLLImport;
-    bool attrDLLExport;
-
     TypePtr type2;
     llvm::GlobalVariable *llGlobal;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
 
+    bool attributesVerified:1;
+    bool attrDLLImport:1;
+    bool attrDLLExport:1;
+
     ExternalVariable(Module *module, Visibility visibility)
         : TopLevelItem(EXTERNAL_VARIABLE, module, visibility),
-          attributes(new ExprList()), attributesVerified(false),
-          llGlobal(NULL), debugInfo(NULL) {}
+          attributes(new ExprList()),
+          llGlobal(NULL), debugInfo(NULL),
+          attributesVerified(false)
+          {}
     ExternalVariable(Module *module, IdentifierPtr name,
                      Visibility visibility,
                      ExprPtr type,
                      ExprListPtr attributes)
         : TopLevelItem(EXTERNAL_VARIABLE, module, name, visibility),
           type(type), attributes(attributes),
-          attributesVerified(false), llGlobal(NULL), debugInfo(NULL) {}
+          llGlobal(NULL), debugInfo(NULL),
+          attributesVerified(false)
+          {}
 
     llvm::DIGlobalVariable getDebugInfo() { return llvm::DIGlobalVariable(debugInfo); }
 };
 
 struct EvalTopLevel : public TopLevelItem {
     ExprListPtr args;
-    bool evaled;
     vector<TopLevelItemPtr> value;
+    bool evaled:1;
 
     EvalTopLevel(Module *module, ExprListPtr args)
         : TopLevelItem(EVAL_TOPLEVEL, module), args(args), evaled(false)
@@ -2351,24 +2385,24 @@ struct Module : public ANode {
 
     EnvPtr env;
     InitState initState;
-    bool attributesVerified;
     vector<llvm::SmallString<16> > attrBuildFlags;
     IntegerTypePtr attrDefaultIntegerType;
     FloatTypePtr attrDefaultFloatType;
 
     llvm::StringMap<ImportSet> publicSymbols;
-    bool publicSymbolsLoaded;
-    int publicSymbolsLoading;
 
     llvm::StringMap<ImportSet> allSymbols;
-    bool allSymbolsLoaded;
-    int allSymbolsLoading;
 
     set<string> importedNames;
 
-    bool topLevelLLVMGenerated;
-    bool externalsGenerated;
-    bool isIntrinsicsModule;
+    int publicSymbolsLoading:3;
+    int allSymbolsLoading:3;
+    bool attributesVerified:1;
+    bool publicSymbolsLoaded:1;
+    bool allSymbolsLoaded:1;
+    bool topLevelLLVMGenerated:1;
+    bool externalsGenerated:1;
+    bool isIntrinsicsModule:1;
 
     llvm::TrackingVH<llvm::MDNode> debugInfo;
 
@@ -2409,7 +2443,7 @@ struct Module : public ANode {
 
 struct PVData {
     TypePtr type;
-    bool isTemp;
+    bool isTemp:1;
     PVData() : type(NULL), isTemp(NULL) {}
     PVData(TypePtr type, bool isTemp)
         : type(type), isTemp(isTemp) {}
@@ -2685,15 +2719,15 @@ struct Type : public Object {
     TypeKind typeKind;
     llvm::Type *llType;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
-    bool defined;
-
-    bool typeInfoInitialized;
     size_t typeSize;
     size_t typeAlignment;
 
-    bool overloadsInitialized;
     vector<OverloadPtr> overloads;
 
+    bool overloadsInitialized:1;
+    bool defined:1;
+    bool typeInfoInitialized:1;
+    
     Type(TypeKind typeKind)
         : Object(TYPE), typeKind(typeKind),
           llType(NULL), debugInfo(NULL), defined(false),
@@ -2708,22 +2742,22 @@ struct BoolType : public Type {
 };
 
 struct IntegerType : public Type {
-    int bits;
-    bool isSigned;
+    int bits:15;
+    bool isSigned:1;
     IntegerType(int bits, bool isSigned)
         : Type(INTEGER_TYPE), bits(bits), isSigned(isSigned) {}
 };
 
 struct FloatType : public Type {
-    int bits;
-    bool isImaginary;
+    int bits:15;
+    bool isImaginary:1;
     FloatType(int bits, bool isImaginary)
         : Type(FLOAT_TYPE), bits(bits), isImaginary(isImaginary){}
 };
 
 struct ComplexType : public Type {
-    int bits;
     const llvm::StructLayout *layout;
+    int bits:15;
     ComplexType(int bits)
         : Type(COMPLEX_TYPE), bits(bits), layout(NULL) {}
 };
@@ -2748,14 +2782,15 @@ struct CodePointerType : public Type {
 };
 
 struct CCodePointerType : public Type {
-    CallingConv callingConv;
     vector<TypePtr> argTypes;
-    bool hasVarArgs;
     TypePtr returnType; // NULL if void return
 
     llvm::Type *callType;
 
     llvm::Type *getCallType();
+
+    CallingConv callingConv:3;
+    bool hasVarArgs:1;
 
     CCodePointerType(CallingConv callingConv,
                      llvm::ArrayRef<TypePtr> argTypes,
@@ -2803,12 +2838,13 @@ struct RecordType : public Type {
     RecordDeclPtr record;
     vector<ObjectPtr> params;
 
-    bool fieldsInitialized;
     vector<IdentifierPtr> fieldNames;
     vector<TypePtr> fieldTypes;
     llvm::StringMap<size_t> fieldIndexMap;
 
     const llvm::StructLayout *layout;
+
+    bool fieldsInitialized:1;
 
     RecordType(RecordDeclPtr record)
         : Type(RECORD_TYPE), record(record), fieldsInitialized(false),
@@ -2822,9 +2858,10 @@ struct VariantType : public Type {
     VariantDeclPtr variant;
     vector<ObjectPtr> params;
 
-    bool initialized;
     vector<TypePtr> memberTypes;
     TypePtr reprType;
+
+    bool initialized:1;
 
     VariantType(VariantDeclPtr variant)
         : Type(VARIANT_TYPE), variant(variant),
@@ -2842,7 +2879,7 @@ struct StaticType : public Type {
 
 struct EnumType : public Type {
     EnumDeclPtr enumeration;
-    bool initialized;
+    bool initialized:1;
 
     EnumType(EnumDeclPtr enumeration)
         : Type(ENUM_TYPE), enumeration(enumeration), initialized(false) {}

compiler/matchinvoke.hpp
@@ -1,5 +1,5 @@
-#ifndef __MATCHIVOKE_HPP
-#define __MATCHIVOKE_HPP
+#ifndef __MATCHINVOKE_HPP
+#define __MATCHINVOKE_HPP
 
 #include "clay.hpp"
 
@@ -27,8 +27,6 @@ struct MatchResult : public Object {
 };
 
 struct MatchSuccess : public MatchResult {
-    bool callByName;
-    InlineAttribute isInline;
     CodePtr code;
     EnvPtr env;
 
@@ -39,7 +37,10 @@ struct MatchSuccess : public MatchResult {
     vector<IdentifierPtr> fixedArgNames;
     IdentifierPtr varArgName;
     vector<TypePtr> varArgTypes;
+
     unsigned varArgPosition;
+    InlineAttribute isInline:2;
+    bool callByName:1;
 
     MatchSuccess(bool callByName, InlineAttribute isInline, CodePtr code, EnvPtr env,
                  ObjectPtr callable, llvm::ArrayRef<TypePtr> argsKey)
@@ -119,4 +120,4 @@ void printMatchError(llvm::raw_ostream &os, const MatchResultPtr& result);
 
 }
 
-#endif // __MATCHIVOKE_HPP
+#endif // __MATCHINVOKE_HPP

compiler/analyzer.hpp
@@ -129,10 +129,10 @@ MultiPValuePtr analyzeCallByName(InvokeEntry* entry,
 void analyzeCodeBody(InvokeEntry* entry);
 
 struct AnalysisContext {
-    bool hasRecursivePropagation;
-    bool returnInitialized;
     vector<uint8_t> returnIsRef;
     vector<TypePtr> returnTypes;
+    bool hasRecursivePropagation:1;
+    bool returnInitialized:1;
     AnalysisContext()
         : hasRecursivePropagation(false),
           returnInitialized(false) {}

compiler/codegen.hpp
@@ -35,7 +35,7 @@ void initExternalTarget(string target);
 struct CValue : public Object {
     TypePtr type;
     llvm::Value *llValue;
-    bool forwardedRValue;
+    bool forwardedRValue:1;
     CValue(TypePtr type, llvm::Value *llValue)
         : Object(CVALUE), type(type), llValue(llValue),
           forwardedRValue(false)
@@ -81,9 +81,9 @@ struct JumpTarget {
 };
 
 struct CReturn {
-    bool byRef;
     TypePtr type;
     CValuePtr value;
+    bool byRef:1;
     CReturn(bool byRef, TypePtr type, CValuePtr value)
         : byRef(byRef), type(type), value(value) {}
 };
@@ -136,18 +136,18 @@ struct CodegenContext {
     vector<JumpTarget> breaks;
     vector<JumpTarget> continues;
     vector<JumpTarget> exceptionTargets;
-    bool checkExceptions;
     llvm::Value *exceptionValue;
-    int inlineDepth;
+    int inlineDepth:31;
+    bool checkExceptions:1;
 
     CodegenContext()
         : llvmFunc(NULL),
           initBuilder(NULL),
           builder(NULL),
           valueForStatics(NULL),
-          checkExceptions(true),
           exceptionValue(NULL),
-          inlineDepth(0)
+          inlineDepth(0),
+          checkExceptions(true)
     {
     }
 
@@ -156,9 +156,9 @@ struct CodegenContext {
           initBuilder(NULL),
           builder(NULL),
           valueForStatics(NULL),
-          checkExceptions(true),
           exceptionValue(NULL),
-          inlineDepth(0)
+          inlineDepth(0),
+          checkExceptions(true)
     {
     }
 

compiler/evaluator.hpp
@@ -8,7 +8,7 @@ namespace clay {
 struct EValue : public Object {
     TypePtr type;
     char *addr;
-    bool forwardedRValue;
+    bool forwardedRValue:1;
     EValue(TypePtr type, char *addr)
         : Object(EVALUE), type(type), addr(addr),
           forwardedRValue(false) {}

compiler/externals.cpp
@@ -296,18 +296,18 @@ struct LLVMExternalTarget : public ExternalTarget {
 struct X86_32_ExternalTarget : public ExternalTarget {
     // Linux, Solaris, and NetBSD pass all aggregates on the stack
     // (except long long and complex float32)
-    bool alwaysPassStructsOnStack;
+    bool alwaysPassStructsOnStack:1;
 
     // Windows x64 has only one calling convention
-    bool alwaysUseCCallingConv;
+    bool alwaysUseCCallingConv:1;
 
     // MacOS X passes XMM vectors in registers; other platforms treat them
     // as aggregates
-    bool passVecAsNonaggregate;
+    bool passVecAsNonaggregate:1;
 
     // MacOS X returns aggregates containing a single float or double
     // on the x87 stack
-    bool returnSingleFloatAggregateAsNonaggregate;
+    bool returnSingleFloatAggregateAsNonaggregate:1;
 
     explicit X86_32_ExternalTarget(llvm::Triple target) {
         alwaysUseCCallingConv = target.getArch() == llvm::Triple::x86_64;
@@ -505,8 +505,10 @@ enum WordClass {
 };
 
 struct X86_64_ExternalTarget : public LLVMExternalTarget {
+    map<TypePtr, vector<WordClass> > typeClassifications;
+    map<TypePtr, llvm::Type*> llvmWordTypes;
     // Mac OS X does its own thing with X87UP chunks not preceded by X87
-    bool passesOrphanX87UPAsSSE;
+    bool passesOrphanX87UPAsSSE:1;
 
     explicit X86_64_ExternalTarget(llvm::Triple target)
         : LLVMExternalTarget(target)
@@ -514,8 +516,6 @@ struct X86_64_ExternalTarget : public LLVMExternalTarget {
         passesOrphanX87UPAsSSE = target.getOS() == llvm::Triple::Darwin;
     }
 
-    map<TypePtr, vector<WordClass> > typeClassifications;
-    map<TypePtr, llvm::Type*> llvmWordTypes;
     llvm::ArrayRef<WordClass> getTypeClassification(TypePtr type);
     llvm::Type* getLLVMWordType(TypePtr type);
 

compiler/hirestimer.hpp
@@ -4,8 +4,8 @@
 namespace clay {
 struct HiResTimer {
     unsigned long long elapsedTicks;
-    int running;
     unsigned long long startTicks;
+    int running;
 
     HiResTimer();
     void start();

compiler/invoketables.hpp
@@ -17,11 +17,6 @@ struct InvokeEntry {
     vector<TypePtr> argsKey;
     vector<uint8_t> forwardedRValueFlags;
 
-    bool analyzed:1;
-    bool analyzing:1;
-    bool callByName:1; // if callByName the rest of InvokeEntry is not set
-    bool runtimeNop:1;
-
     CodePtr origCode;
     CodePtr code;
     EnvPtr env;
@@ -43,6 +38,11 @@ struct InvokeEntry {
     llvm::Function *llvmCWrappers[CC_Count];
 
     llvm::TrackingVH<llvm::MDNode> debugInfo;
+
+    bool analyzed:1;
+    bool analyzing:1;
+    bool callByName:1; // if callByName the rest of InvokeEntry is not set
+    bool runtimeNop:1;
 
     InvokeEntry(InvokeSet *parent,
                 ObjectPtr callable,
@@ -70,12 +70,12 @@ struct InvokeSet {
     vector<OverloadPtr> overloads;
 
     vector<MatchSuccessPtr> matches;
-    unsigned nextOverloadIndex;
-
-    bool shouldLog;
-
     map<vector<ValueTempness>, InvokeEntry*> tempnessMap;
     map<vector<ValueTempness>, InvokeEntry*> tempnessMap2;
+
+    unsigned nextOverloadIndex:31;
+
+    bool shouldLog:1;
 
     InvokeSet(ObjectPtr callable,
               llvm::ArrayRef<TypePtr> argsKey,
@@ -94,7 +94,7 @@ typedef vector< pair<OverloadPtr, MatchResultPtr> > MatchFailureVector;
 
 struct MatchFailureError {
     MatchFailureVector failures;
-    bool failedInterface;
+    bool failedInterface:1;
 
     MatchFailureError() : failedInterface(false) {}
 };

commit d88f145a50a0c538c09a55be5062a4b7b536f63b

Fix compiler on Windows
Broken in commit 4ed588f07aaefd487adaa55dc2552bad88411f83.

compiler/clay.hpp
@@ -1511,7 +1511,7 @@ enum ReturnKind {
 
 struct Return : public Statement {
     ExprListPtr values;
-    ReturnKind returnKind:2;
+    ReturnKind returnKind:3;
     bool isExprReturn:1;
     bool isReturnSpecs:1;
     Return(ReturnKind returnKind, ExprListPtr values)
@@ -1965,7 +1965,7 @@ struct Overload : public TopLevelItem {
     PatternPtr callablePattern;
     vector<PatternPtr> argPatterns;
     MultiPatternPtr varArgPattern;
-    InlineAttribute isInline:2;
+    InlineAttribute isInline:3;
     int patternsInitializedState:2; // 0:notinit, -1:initing, +1:inited
     bool callByName:1;
     bool nameIsPattern:1;
@@ -2126,7 +2126,7 @@ struct ExternalProcedure : public TopLevelItem {
     llvm::Function *llvmFunc;
     llvm::TrackingVH<llvm::MDNode> debugInfo;
 
-    CallingConv callingConv:3;
+    CallingConv callingConv:4;
     bool hasVarArgs:1;
     bool attributesVerified:1;
     bool attrDLLImport:1;

compiler/matchinvoke.hpp
@@ -39,7 +39,7 @@ struct MatchSuccess : public MatchResult {
     vector<TypePtr> varArgTypes;
 
     unsigned varArgPosition;
-    InlineAttribute isInline:2;
+    InlineAttribute isInline:3;
     bool callByName:1;
 
     MatchSuccess(bool callByName, InlineAttribute isInline, CodePtr code, EnvPtr env,